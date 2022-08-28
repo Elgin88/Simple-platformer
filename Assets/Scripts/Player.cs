51d9caa763215ba753a95ba3fa8d569ea9448a72
Elgin88 <elgin-88@mail.ru>
2022-08-28 21:20:53 +0800

    1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private int _health;
+    [SerializeField] private float _speed;
+
+    private int _currentHealth;
+    private string _moveLeft;
+    private string _moveRight;
+
+    private bool _isLeft = false;
+    private bool _isRight = false;
+
+    public event UnityAction<int> HealthChanged;
+
+    public void Start()
     {
-        
+        _currentHealth = _health;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Update()
     {
-        
+        if (Input.GetKey(KeyCode.D))
+        {
+            _isRight = true;
+            _isLeft = false;
+            transform.Translate(_speed * Time.deltaTime, 0,0);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            _isLeft = true;
+            _isRight = false;
+            transform.Translate(_speed * Time.deltaTime * -1, 0, 0);
+        }
+    }
+
+    private IEnumerator MoveLeft()
+    {
+        yield return null;
     }
 }
