82671767f7b2b8dd40c19ef1b3f66ff8a777db6b
Elgin88 <elgin-88@mail.ru>
2022-08-28 22:44:40 +0800

    1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody2D))]
+
 public class Player : MonoBehaviour
 {
     [SerializeField] private int _health;
     [SerializeField] private float _speed;
+    [SerializeField] private float _force;
+    [SerializeField] private float _delayAfterJump;
 
+    private Rigidbody2D _rigidbody;
     private int _currentHealth;
     private string _moveLeft;
     private string _moveRight;
+    private float _timeAfterLastJump;
 
     private bool _isLeft = false;
     private bool _isRight = false;
@@ -20,10 +27,13 @@ public class Player : MonoBehaviour
     public void Start()
     {
         _currentHealth = _health;
+        _rigidbody = GetComponent<Rigidbody2D>();
     }
 
     public void Update()
     {
+        _timeAfterLastJump += Time.deltaTime;
+
         if (Input.GetKey(KeyCode.D))
         {
             _isRight = true;
@@ -36,10 +46,13 @@ public class Player : MonoBehaviour
             _isRight = false;
             transform.Translate(_speed * Time.deltaTime * -1, 0, 0);
         }
-    }
-
-    private IEnumerator MoveLeft()
-    {
-        yield return null;
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (_timeAfterLastJump > _delayAfterJump)
+            {
+                _rigidbody.AddForce(Vector2.up * _force, ForceMode2D.Force);
+                _timeAfterLastJump = 0;
+            }
+        }
     }
 }
