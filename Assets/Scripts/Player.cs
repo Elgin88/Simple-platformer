c1fca40ffea5a1ae6b067bd4cb2b020e5ae3d105
Elgin88 <elgin-88@mail.ru>
2022-08-29 11:48:47 +0800

    1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,56 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-[RequireComponent(typeof(Animator))]
-[RequireComponent(typeof(Rigidbody2D))]
-
 public class Player : MonoBehaviour
 {
-    [SerializeField] private int _health;
-    [SerializeField] private float _speed;
-    [SerializeField] private float _force;
-    [SerializeField] private float _delayAfterJump;
-
-    private Rigidbody2D _rigidbody;
+    [SerializeField] private int _health = 100;
+    
     private int _currentHealth;
-    private string _moveLeft;
-    private string _moveRight;
-    private float _timeAfterLastJump;
-
-    private bool _isLeft = false;
-    private bool _isRight = false;
+    private int _numberOfCoins;
 
     public event UnityAction<int> HealthChanged;
+    public event UnityAction<int> NumberOfCoinsChanged;
+    public event UnityAction IsDead;
 
     public void Start()
     {
         _currentHealth = _health;
-        _rigidbody = GetComponent<Rigidbody2D>();
     }
 
-    public void Update()
+    public void TakeDamage(int damage)
     {
-        _timeAfterLastJump += Time.deltaTime;
+        _currentHealth -= damage;
+        HealthChanged?.Invoke(_currentHealth);
 
-        if (Input.GetKey(KeyCode.D))
+        if (_currentHealth <= 0)
         {
-            _isRight = true;
-            _isLeft = false;
-            transform.Translate(_speed * Time.deltaTime, 0,0);
+            Destroy(gameObject);
+            IsDead?.Invoke();
         }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            _isLeft = true;
-            _isRight = false;
-            transform.Translate(_speed * Time.deltaTime * -1, 0, 0);
-        }
-        if (Input.GetKey(KeyCode.Space))
-        {
-            if (_timeAfterLastJump > _delayAfterJump)
-            {
-                _rigidbody.AddForce(Vector2.up * _force, ForceMode2D.Force);
-                _timeAfterLastJump = 0;
-            }
-        }
+    }
+
+    public void TakeCoin()
+    {
+        _numberOfCoins++;
+        NumberOfCoinsChanged?.Invoke(_numberOfCoins);        
     }
 }
